22643796facd6d6d50480bdb3484aac04aecce87
codewithemmy <codewithemmy@gmail.com>
2024-03-06 14:46:15 +0100

    new changes

--- files/student/student.model.js
+++ files/student/student.model.js
@@ -83,6 +83,7 @@ const studentSchema = new mongoose.Schema(
     password: { type: String },
     accountType: {
       type: String,
+      enum: ["student"]
     },
     isDelete: {
       type: Boolean,
--- files/user/user.model.js
+++ files/user/user.model.js
@@ -83,6 +83,7 @@ const userSchema = new mongoose.Schema(
     password: { type: String },
     accountType: {
       type: String,
+      enum: ["teacher"]
     },
     isDelete: {
       type: Boolean,
