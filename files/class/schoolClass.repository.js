75c7d2792f76623d68284fc06d84ab473905aedd
codewithemmy <codewithemmy@gmail.com>
2024-03-21 15:09:39 +0100

    new update

--- files/class/schoolClass.repository.js
+++ files/class/schoolClass.repository.js
@@ -24,8 +24,6 @@ class SchoolClassRepository {
     const { limit, skip, sort, ...restOfPayload } = payload
 
     const schoolClass = await SchoolClass.find({ ...restOfPayload })
-      .populate({ path: "teacher" })
-      .populate({ path: "student" })
       .sort(sort)
       .skip(skip)
       .limit(limit)
