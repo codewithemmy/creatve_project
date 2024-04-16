840b57a40029ae5ce6455c22c390c33a450196e7
codewithemmy <codewithemmy@gmail.com>
2024-04-16 11:10:22 +0100

    new update

--- files/record/record.model.js
+++ files/record/record.model.js
@@ -26,31 +26,37 @@ const recordSchema = new mongoose.Schema(
   },
   { timestamps: true }
 )
-
 // Middleware to recalculate totalScore before update
-recordSchema.pre("findOneAndUpdate", function (next) {
-  const update = this.getUpdate()
-  // Calculate totalScore based on updated values
-  const updatedTestOne =
-    update.resumptionTest !== undefined
-      ? update.resumptionTest
-      : this.getQuery().$set?.resumptionTest
-  const updatedTestTwo =
-    update.midTermTest !== undefined
-      ? update.midTermTest
-      : this.getQuery().$set?.midTermTest
-  const updatedTestThree =
-    update.project !== undefined ? update.project : this.getQuery().$set.project
-  const updatedExamScore =
-    update.examScore !== undefined
-      ? update.examScore
-      : this.getQuery().$set?.examScore
-  update.totalScore =
-    (updatedTestOne || 0) +
-    (updatedTestTwo || 0) +
-    (updatedTestThree || 0) +
-    (updatedExamScore || 0)
-  next()
+recordSchema.pre("findOneAndUpdate", async function () {
+  try {
+    const docToUpdate = await this.model.findOne(this.getQuery())
+    const update = this.getUpdate()
+
+    // Calculate totalScore based on existing and updated values
+    const updatedResumptionTest =
+      update.resumptionTest !== undefined
+        ? update.resumptionTest
+        : docToUpdate.resumptionTest
+    const updatedMidTermTest =
+      update.midTermTest !== undefined
+        ? update.midTermTest
+        : docToUpdate.midTermTest
+    const updatedProject =
+      update.project !== undefined ? update.project : docToUpdate.project
+    const updatedExamScore =
+      update.examScore !== undefined ? update.examScore : docToUpdate.examScore
+
+    const totalScore =
+      (updatedResumptionTest || 0) +
+      (updatedMidTermTest || 0) +
+      (updatedProject || 0) +
+      (updatedExamScore || 0)
+
+    // Update the totalScore in the update object
+    update.totalScore = totalScore
+  } catch (error) {
+    console.error("Error in pre findOneAndUpdate middleware:", error)
+  }
 })
 
 const record = mongoose.model("Record", recordSchema, "record")
